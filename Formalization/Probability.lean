f24e35f099a350ecff993208e67ef56f26c31a4d
mashen <mashen@student.ethz.ch>
2025-11-15 13:19:38 +0100

    bruhmetheus

--- Formalization/Probability.lean
+++ Formalization/Probability.lean
@@ -1,4 +1,11 @@
 import Mathlib.Tactic
+open MeasureTheory ProbabilityTheory
 
 set_option autoImplicit false
--- set_option tactc.hygenic false
+set_option linter.style.commandStart false
+
+variable {Ω : Type*} [MeasurableSpace Ω] {P : Measure Ω} [IsProbabilityMeasure P]
+variable {X : Ω → ℕ} (hX : Measurable X)
+
+#check MeasurableSet.of_discrete
+
