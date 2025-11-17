150b5208fcbbcc671e077416daf70eb6d120c451
mashen <mashen@student.ethz.ch>
2025-11-17 17:42:38 +0100

    Added example in Probability.lean

--- Formalization/Probability.lean
+++ Formalization/Probability.lean
@@ -1,5 +1,6 @@
-import Mathlib.Tactic
+import Mathlib
 open MeasureTheory ProbabilityTheory
+open scoped ENNReal
 
 set_option autoImplicit false
 set_option linter.style.commandStart false
@@ -7,5 +8,18 @@ set_option linter.style.commandStart false
 variable {Ω : Type*} [MeasurableSpace Ω] {P : Measure Ω} [IsProbabilityMeasure P]
 variable {X : Ω → ℕ} (hX : Measurable X)
 
-#check MeasurableSet.of_discrete
+structure Probability (α : Type*) where
+ EventSpace     : MeasurableSpace α
+ P              : Measure α
 
+#check IsProbabilityMeasure
+
+/- # COIN TOSS #
+   coin toss using bools for sides.
+   true = heads, false = tails
+   TODO: find out how to define instance of a Measure.
+-/
+def coin_toss : Probability Bool := {
+    EventSpace := Bool.instMeasurableSpace,
+    P := sorry
+}
