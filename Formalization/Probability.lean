f26d4b61f2ce4828acb49ecc575f06f8d588b37d
mashen <mashen@student.ethz.ch>
2025-11-22 14:15:42 +0100

    for pulling

--- Formalization/Probability.lean
+++ Formalization/Probability.lean
@@ -1,20 +1,18 @@
-import Mathlib
-open MeasureTheory ProbabilityTheory SimpleGraph
+import Mathlib.Tactic
+open MeasureTheory ProbabilityTheory ProbabilityTheory
 open scoped ENNReal
 
 set_option autoImplicit false
 set_option linter.style.commandStart false
 
 variable {Ω : Type*} [MeasurableSpace Ω] {P : Measure Ω} [IsProbabilityMeasure P]
-variable {X : Ω → ℕ} (hX : Measurable X)
 
 /- # General Probability Structure #
 This structure is for defining general probability structures.
 Use def to create an instance of it.  -/
 structure Probability (α : Type*) where
  EventSpace     : MeasurableSpace α
- P              : Measure α
- isProbability  : IsProbabilityMeasure P
+ P              : PMF α
 /- EXAMPLE: COIN THROW -/
 inductive coin where
 | head
@@ -22,8 +20,7 @@ inductive coin where
 def coinOmega1 : MeasurableSpace coin := MeasurableSpace.generateFrom {{coin.head},{coin.tail}}
 instance coin_toss : Probability coin := {
     EventSpace := coinOmega1,
-    P := sorry,
-    isProbability := (by sorry)
+    P := sorry
 }
 
 #check IsProbabilityMeasure
