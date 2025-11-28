9784ba4b8dfdfc66b51d581bfdfc0a4698a7744e
mashen <mashen@student.ethz.ch>
2025-11-28 14:39:29 +0100

    Defined Probability Space :: missing two more conditions

--- Formalization/Probability.lean
+++ Formalization/Probability.lean
@@ -14,22 +14,20 @@ abbrev graph (n : ℕ) := SimpleGraph (Fin n)
 Here we setup the basic probability notion, in conjnction with Graphs, that will be needed for
 the proof.
 -/
-
 abbrev pr_bound (p : ℝ≥0) := 0 ≤ p ∧ p ≤ 1
 
 def Dist_Bernoulli (b : Bool)(p : ℝ≥0)(_ : pr_bound p) : ℝ≥0 := if b then p else 1 - p
 
 /- Probability Space Definition (General) -/
-structure PrSpace (Ω : Set α)[Finite Ω] where
+structure PrSpace (Ω : Set α) where
     Sa := Ω
-    Ev := Ω.powerset -- overwriteable
+    Ev : Set (Set α)
     Pr : Ev → ℝ≥0
     -- General conditions
-    Sa_c : Sa = Ω           -- ENSURES that sample space is the passed argument
-    Ev_c : Ev ⊆ Ω.powerset  -- ENSURES that any Event Space is a subset of the powerset
+    Ev_c : Ev ⊆ Sa.powerset  -- ENSURES that any Event Space is a subset of the powerset
     /- CONDITIONS FOR THE EVENT SPACE -/
     -- Everything happening is a Event
-    base : Ω ∈ Ev
+    base : Sa ∈ Ev
     /-  The complement of an Event is still an Event
         Here adapted to treat the set as universe
         Might be a smarter way to do this... (Somhow treat it as a type) -/
@@ -42,25 +40,46 @@ structure PrSpace (Ω : Set α)[Finite Ω] where
     -- Pr ∅ = 0
     zero :
         Pr ⟨∅,by
-        subst Sa_c;apply compl at base
+        apply compl at base
         have null : {x | x ∈ Sa ∧ x ∉ Sa} = ∅ := by {
             simp only [and_not_self, Set.setOf_false]
-        }; rw [←null]; trivial
+        }; rw [←null];trivial
         ⟩ = 0 -- Real annoying that I had to show this explicitly
     -- TODO (The probability of the union of DISJOINT Events
     --      is the same as the sum of their singular probabilities)
     additive : sorry
 
 /- Probability Space Definition (Graph) -/
-def PrSpace_G {n : ℕ}(G : graph n) : PrSpace G.edgeSet where
-    Sa_c := by rfl
+noncomputable
+def PrSpace_G {n : ℕ}(G : graph n)(p : ℝ≥0)(bound : p ≤ 1) : PrSpace G.edgeSet where
+    Sa := G.edgeSet
+    Ev := G.edgeSet.powerset
     Ev_c := by rfl
-    Pr := sorry
+    -- Check if sound!
+    Pr := fun A ↦ if A.val.ncard = 0 then 0
+        else p^(A.val.ncard) * (1-p)^({ a ∈ G.edgeSet | a ∉ A.val }.ncard)
     base := by simp only [Set.mem_powerset_iff, subset_refl]
     compl := by simp only [Set.mem_powerset_iff, Set.sep_subset, implies_true]
     union := sorry
-    bound := sorry
-    zero := sorry
+    bound := by
+        simp only [zero_le, true_and, Subtype.forall, Set.mem_powerset_iff]
+        intro A h
+        split_ifs with cif
+        · simp only [zero_le]
+        · generalize SimpleGraph.edgeSet G = B at *
+          have leOne : ∀(p : ℝ≥0), ∀x, p ≤ 1 → p^x ≤ 1 := by {
+            intros p' n' h'
+            induction n'
+            · simp only [pow_zero, le_refl]
+            · rw [@npow_add]; norm_num
+              rename_i n'' IH
+              grw[IH, h']; norm_num
+          }
+          grw [leOne, leOne]
+          · simp only [mul_one, le_refl]
+          · simp only [tsub_le_iff_right, le_add_iff_nonneg_right, zero_le]
+          · assumption
+    zero := by simp only [Set.ncard_empty, ↓reduceIte]
     additive := sorry
 
 /- THE PLAN
